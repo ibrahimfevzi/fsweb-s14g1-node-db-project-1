3ce5c7d456c3907717bc43c3982fcf422a74a8dd
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-05-22 17:13:58 +0300

    Api yazıldı

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,66 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res, next) => {
-  // KODLAR BURAYA
-})
+const Account = require("./accounts-model");
+const mw = require("./accounts-middleware");
 
-router.get('/:id', (req, res, next) => {
-  // KODLAR BURAYA
-})
-
-router.post('/', (req, res, next) => {
-  // KODLAR BURAYA
-})
-
-router.put('/:id', (req, res, next) => {
-  // KODLAR BURAYA
+router.get("/", async (req, res, next) => {
+  try {
+    const accounts = await Account.getAll();
+    res.json(accounts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // KODLAR BURAYA
-})
+router.get("/:id", mw.checkAccountId, async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id);
+    res.json(account);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // KODLAR BURAYA
-})
+router.post(
+  "/",
+  mw.checkAccountPayload,
+  mw.checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const account = await Account.create(req.body);
+      res.status(201).json(account);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  mw.checkAccountId,
+  mw.checkAccountPayload,
+  async (req, res, next) => {
+    try {
+      const account = await Account.updateById(req.params.id, req.body);
+      res.json(account);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete("/:id", mw.checkAccountId, async (req, res, next) => {
+  try {
+    const account = await Account.deleteById(req.params.id);
+    res.json(account);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: "Server error" });
+});
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const server = express();
 
+const router = require("./accounts/accounts-router");
+
 server.use(express.json());
+
+server.use("/api/accounts", router);
 
 module.exports = server;
